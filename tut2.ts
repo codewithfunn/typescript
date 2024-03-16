e249e2917f428916797cc1477c5eb3b65452ff63
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-03-17 01:40:17 +0530

    tut2 . create a simple add program and init the ts config file

---
+++ tut2.ts
@@ -0,0 +1,11 @@
+// TODO 1: Write a program to add two numbers?
+// TODO 2: How to catch Errors & solve it?
+// TODO 3: TS Configuration File
+
+// func define
+// @ts-ignore
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(sum(5, 6));
