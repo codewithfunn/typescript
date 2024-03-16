b80866130db970c6b01a036b53395eebc955f300
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-03-17 01:04:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("I am shine-codders");
+var num = 5;
+// num ="shine-codder";
+num = 10;
+console.log(num);
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("I am shine-codders");
+let num: number = 5;
+// num ="shine-codder";
+num = 10;
+console.log(num);
